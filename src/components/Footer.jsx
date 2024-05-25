ca416c65160c6e6727e54b4edaa9a7af47423907
b1gmcdaddy <bobtangpuz@gmail.com>
2024-05-25 18:31:13 +0800

    mdsf

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="bg-gray-800 text-white py-5">
       <div className="max-w-[1240px] mx-auto text-center">
-        <p>&copy; {new Date().getFullYear()} Bazura Grill. All rights reserved.</p>
+        <p>&copy; {new Date().getFullYear()} Portfolio. All rights reserved.</p>
         <p>Developed by Jolony Tim N. Tangpuz</p>
       </div>
     </footer>
