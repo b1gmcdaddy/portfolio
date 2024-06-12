e8fcc92bde21b77cdb2137977e1b54c264d68ae8
b1gmcdaddy <bobtangpuz@gmail.com>
2024-06-12 19:48:41 +0800

    added: project technologies used

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -35,13 +35,14 @@ const Projects = () => {
       transition={{ duration: 0.8, delay: 0.5 }}
     >
       <div className='max-w-[1240px] mx-auto xs:px-10 text-center'>
-        <h1 className='text-white md:text-4xl sm:text-2xl tracking-wide text-xl font-bold'>My Projects</h1>
+        <h1 className='text-white md:text-4xl xs:text-4xl tracking-wide font-bold'>My Projects</h1>
         <div className='w-[100px] h-[4px] mx-auto bg-sky-300 rounded-md mt-4 mb-12'></div>
       </div>
         <div className='max-w-[1240px] mx-auto grid md:grid-cols-3 gap-16'>
         <div className='w-full shadow-xl flex flex-col p-4 rounded-lg bg-slate-800'>
           <img className='mx-auto rounded-md mt-[-1.5rem] hover:scale-105 duration-300 w-full h-48' src={carolhub} alt='/' />
-          <h2 className='text-2xl font-bold px-4 py-5'>CarolHub</h2>
+          <h2 className='text-2xl font-bold px-4 mt-5'>CarolHub</h2>
+          <p className='text-[14px] px-4 mb-5 italic text-sky-300'>ReactJS, TailwindCSS, FireBase</p>
           <p className='text-base px-4'>A chat app with Google Sign-In and dynamic chat rooms. </p>
           <a href='https://carol-hub.web.app/' className='flex justify-end px-4 py-2'>
             <FontAwesomeIcon icon={faGithub} className='text-2xl hover:text-sky-300' />
@@ -49,7 +50,8 @@ const Projects = () => {
         </div>
         <div className='w-full shadow-xl flex flex-col p-4 rounded-lg bg-slate-800'>
           <img className='mx-auto rounded-md mt-[-1.5rem] hover:scale-105 duration-300 w-full h-48' src={bazura} alt='/' />
-          <h2 className='text-2xl font-bold px-4 py-5'>BAZURA</h2>
+          <h2 className='text-2xl font-bold px-4 mt-5'>BAZURA</h2>
+          <p className='text-[14px] px-4 mb-5 italic text-sky-300'>ReactJS, TailwindCSS, NodeJS, Framer-Motion, MySQL, theMealDB API </p>
           <p className='text-base px-4'>A showcasing website for the Bazura Grill with CRUD capabilities for the admin.</p>
           <a href='https://github.com/b1gmcdaddy/bazura' className='flex justify-end px-4 py-2'>
             <FontAwesomeIcon icon={faGithub} className='text-2xl hover:text-sky-300' />
@@ -57,7 +59,8 @@ const Projects = () => {
         </div>
         <div className='w-full shadow-xl flex flex-col p-4 rounded-lg bg-slate-800'>
           <img className='mx-auto rounded-md mt-[-1.5rem] hover:scale-105 duration-300 w-full h-48' src={foodtrips} alt='/' />
-          <h2 className='text-2xl font-bold px-4 py-5'>Restos v.3.0.0</h2>
+          <h2 className='text-2xl font-bold px-4 mt-5'>Restos v.3.0.0</h2>
+          <p className='text-[14px] px-4 mb-5 italic text-sky-300'>ReactJS, ChakraUI, NodeJS, MySQL</p>
           <p className='text-base px-4'>A restaurant note tracking app to keep track of potential foodtrips.</p>
           <a href='https://github.com/b1gmcdaddy/jolo-restos' className='flex justify-end px-4 py-2'>
             <FontAwesomeIcon icon={faGithub} className='text-2xl hover:text-sky-300' />
@@ -65,7 +68,8 @@ const Projects = () => {
         </div>
         <div className='w-full shadow-xl flex flex-col p-4 rounded-lg bg-slate-800'>
           <img className='mx-auto rounded-md mt-[-1.5rem] hover:scale-105 duration-300 w-full h-48' src={saad} alt='/' />
-          <h2 className='text-2xl font-bold px-4 py-5'>DCISM Inventory System</h2>
+          <h2 className='text-2xl font-bold px-4 mt-5'>DCISM Inventory System</h2>
+          <p className='text-[14px] px-4 mb-5 italic text-sky-300'>ReactJS, ChakraUI, NodeJS, MySQL</p>
           <p className='text-base px-4'>Final Project for CIS 2201. Inventory System for the CS/IT/IS Department.</p>
           <a href='https://github.com/b1gmcdaddy/CIS-2201---Inventory-System' className='flex justify-end px-4 py-2'>
             <FontAwesomeIcon icon={faGithub} className='text-2xl hover:text-sky-300' />
@@ -73,7 +77,8 @@ const Projects = () => {
         </div>
         <div className='w-full shadow-xl flex flex-col p-4 rounded-lg bg-slate-800'>
           <img className='mx-auto rounded-md mt-[-1.5rem] hover:scale-105 duration-300 w-full h-48' src={interasian} alt='/' />
-          <h2 className='text-2xl font-bold px-4 py-5'>Real Estate Website</h2>
+          <h2 className='text-2xl font-bold px-4 mt-5'>Real Estate Website</h2>
+          <p className='text-[14px] px-4 mb-5 italic text-sky-300'>PHP, JavaScript, MySQL</p>
           <p className='text-base px-4'>A website that displays real-estate properties through CRUD functionalities.</p>
           <a href='https://github.com/b1gmcdaddy/Interasian-Realty-Services' className='flex justify-end px-4 py-2'>
             <FontAwesomeIcon icon={faGithub} className='text-2xl hover:text-sky-300' />
@@ -81,7 +86,8 @@ const Projects = () => {
         </div>
         <div className='w-full shadow-xl flex flex-col p-4 rounded-lg bg-slate-800'>
           <img className='mx-auto rounded-md mt-[-1.5rem] hover:scale-105 duration-300 w-full h-48' src={parokyano} alt='/' />
-          <h2 className='text-2xl font-bold px-4 py-5'>PAROKYANO <span className='text-gray-400 text-sm'>(Ongoing)</span></h2>
+          <h2 className='text-2xl font-bold px-4 mt-5'>PAROKYANO <span className='text-gray-400 text-sm'>(Ongoing)</span></h2>
+          <p className='text-[14px] px-4 mb-5 italic text-sky-300'>ReactJS, MaterialUI, NodeJS, Python, MySQL</p>
           <p className='text-base px-4'>A web-based, mobile responsive church management system.</p>
           <a href='' className='flex justify-end px-4 py-2'>
             <FontAwesomeIcon icon={faGithub} className='text-2xl hover:text-sky-300' />
