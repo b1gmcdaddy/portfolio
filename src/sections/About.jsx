50a85da1c3d9a43f2a444a3910d586068bbadb71
b1gmcdaddy <bobtangpuz@gmail.com>
2025-01-10 20:20:30 +0800

    updated resume

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,70 +1,88 @@
-import React, { useRef } from 'react';
-import me2 from '../assets/images/me2.png';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faDownload, faContactBook } from "@fortawesome/free-solid-svg-icons";
-import { faFacebook, faInstagram, faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
-import { Link } from "react-scroll";
-import { motion, useInView } from 'framer-motion';
+import React, {useRef} from "react";
+import me2 from "../assets/images/me2.png";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faDownload, faContactBook} from "@fortawesome/free-solid-svg-icons";
+import {
+  faFacebook,
+  faInstagram,
+  faLinkedin,
+  faGithub,
+} from "@fortawesome/free-brands-svg-icons";
+import {Link} from "react-scroll";
+import {motion, useInView} from "framer-motion";
 
 const About = () => {
   const ref = useRef(null);
-  const isInView = useInView(ref, { once: false, margin: '0px 0px -50px 0px' });
+  const isInView = useInView(ref, {once: false, margin: "0px 0px -50px 0px"});
 
   return (
-    <div 
-      className='w-full h-screen py-24 px-4 mt-20 text-white md:text-justify xs:text-center'
-    >
-      <div ref={ref} className='max-w-[1240px] mx-auto grid md:grid-cols-2'>
-        <motion.img 
-          className='md:w-[500px] md:h-[500px] xs:w-[350px] xs:h-[350px] mx-auto my-4 rounded-full border-solid border-[8px] border-slate-800 shadow-lg shadow-slate-400' 
+    <div className="w-full h-screen py-24 px-4 mt-20 text-white md:text-justify xs:text-center">
+      <div ref={ref} className="max-w-[1240px] mx-auto grid md:grid-cols-2">
+        <motion.img
+          className="md:w-[500px] md:h-[500px] xs:w-[350px] xs:h-[350px] mx-auto my-4 rounded-full border-solid border-[8px] border-slate-800 shadow-lg shadow-slate-400"
           src={me2}
-          style={{ borderRightWidth: '32px', borderBottomWidth: '32px' }} 
-          initial={{ x: -200, opacity: 0 }}
-          animate={isInView ? { x: 0, opacity: 1 } : { x: -200, opacity: 0 }}
-          transition={{ duration: 1.1 }}
+          style={{borderRightWidth: "32px", borderBottomWidth: "32px"}}
+          initial={{x: -200, opacity: 0}}
+          animate={isInView ? {x: 0, opacity: 1} : {x: -200, opacity: 0}}
+          transition={{duration: 1.1}}
         />
-        <motion.div 
-          className='flex flex-col justify-center'
-          initial={{ x: 200, opacity: 0 }}
-          animate={isInView ? { x: 0, opacity: 1 } : { x: 200, opacity: 0 }}
-          transition={{ duration: 1.5 }}
-        >
-          <h1 className='md:text-5xl xs:text-3xl font-[Tahoma] md:tracking-wider font-bold py-4'>Jolony Tim N. Tangpuz</h1>
-          <div className='md:w-[170px] xs:w-[100px] h-[4px] bg-sky-300 rounded-md mb-5 md:mx-0 xs:mx-auto'></div>
-          <p className='md:text-justify mb-6 md:text-2xl xs:text-lg xs:text-center ml-2'>
-            Aspiring Full-Stack Software Developer
+        <motion.div
+          className="flex flex-col justify-center"
+          initial={{x: 200, opacity: 0}}
+          animate={isInView ? {x: 0, opacity: 1} : {x: 200, opacity: 0}}
+          transition={{duration: 1.5}}>
+          <h1 className="md:text-5xl xs:text-3xl font-[Tahoma] md:tracking-wider font-bold py-4">
+            Jolony Tim N. Tangpuz
+          </h1>
+          <div className="md:w-[170px] xs:w-[100px] h-[4px] bg-sky-300 rounded-md mb-5 md:mx-0 xs:mx-auto"></div>
+          <p className="md:text-justify mb-6 md:text-2xl xs:text-lg xs:text-center ml-2">
+            Full-Stack Software Developer
           </p>
-          <div className='flex space-x-8 mb-6 md:ml-2 md:justify-start xs:justify-center'>
-            <a href='https://www.facebook.com/profile.php?id=100005535220914' className='text-white hover:text-gray-400'>
-              <FontAwesomeIcon icon={faFacebook} size='2x' />
+          <div className="flex space-x-8 mb-8 md:ml-2 md:justify-start xs:justify-center">
+            <a
+              href="https://www.facebook.com/profile.php?id=100005535220914"
+              className="text-white hover:text-gray-400">
+              <FontAwesomeIcon icon={faFacebook} size="2x" />
             </a>
-            <a href='https://www.instagram.com/jolotangpuz/' className='text-white hover:text-gray-400'>
-              <FontAwesomeIcon icon={faInstagram} size='2x' />
+            <a
+              href="https://www.instagram.com/jolotangpuz/"
+              className="text-white hover:text-gray-400">
+              <FontAwesomeIcon icon={faInstagram} size="2x" />
             </a>
-            <a href='https://www.linkedin.com/in/jolony-tim-tangpuz-53b529214/' className='text-white hover:text-gray-400'>
-              <FontAwesomeIcon icon={faLinkedin} size='2x' />
+            <a
+              href="https://www.linkedin.com/in/jolony-tim-tangpuz-53b529214/"
+              className="text-white hover:text-gray-400">
+              <FontAwesomeIcon icon={faLinkedin} size="2x" />
             </a>
-            <a href='https://github.com/b1gmcdaddy' className='text-white hover:text-gray-400'>
-              <FontAwesomeIcon icon={faGithub} size='2x' />
+            <a
+              href="https://github.com/b1gmcdaddy"
+              className="text-white hover:text-gray-400">
+              <FontAwesomeIcon icon={faGithub} size="2x" />
             </a>
           </div>
-          <span className='font-medium'>
-            <Link to='contact'  spy={true}
-                                smooth={true}
-                                offset={-70}
-                                duration={300} className='border-solid mr-6 rounded-full py-3 px-5 bg-neutral-200 cursor-pointer hover:bg-neutral-100 text-black'>
+          <span className="font-medium">
+            <Link
+              to="contact"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={300}
+              className="border-solid mr-6 rounded-full py-3 px-5 bg-neutral-200 cursor-pointer hover:bg-neutral-100 text-black">
               Contact Me
-              <FontAwesomeIcon icon={faContactBook} className='ml-2' />
+              <FontAwesomeIcon icon={faContactBook} className="ml-2" />
             </Link>
-            <a href='/JOLONY TIM TANGPUZ - CV.pdf' download className='border-solid rounded-full py-3 px-5 bg-sky-700 hover:bg-sky-600'>
-              Download CV
-              <FontAwesomeIcon icon={faDownload} className='ml-2' />
+            <a
+              href="/JOLONY TIM N. TANGPUZ RESUME.pdf"
+              download
+              className="border-solid rounded-full py-3 px-5 bg-sky-700 hover:bg-sky-600">
+              Download Resume
+              <FontAwesomeIcon icon={faDownload} className="ml-2" />
             </a>
           </span>
         </motion.div>
       </div>
     </div>
   );
-}
+};
 
 export default About;
--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,17 +1,17 @@
-import React, { useEffect, useRef } from 'react';
-import interasian from '../assets/images/interasian.jpg';
-import carolhub from '../assets/images/carolhub.jpg';
-import bazura from '../assets/images/bazura.jpg';
-import restos from '../assets/images/restos.jpg';
-import parokyano from '../assets/images/parokyano.jpg';
-import saad from '../assets/images/saad.jpg';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub } from "@fortawesome/free-brands-svg-icons";
-import { motion, useInView, useAnimation } from 'framer-motion';
+import React, {useEffect, useRef} from "react";
+import interasian from "../assets/images/interasian.jpg";
+import carolhub from "../assets/images/carolhub.jpg";
+import bazura from "../assets/images/bazura.jpg";
+import restos from "../assets/images/restos.jpg";
+import parokyano from "../assets/images/parokyano.jpg";
+import saad from "../assets/images/saad.jpg";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faGithub} from "@fortawesome/free-brands-svg-icons";
+import {motion, useInView, useAnimation} from "framer-motion";
 
 const Projects = () => {
   const ref = useRef(null);
-  const isInView = useInView(ref, { margin: '0px 0px -50px 0px' });
+  const isInView = useInView(ref, {margin: "0px 0px -50px 0px"});
   const mainControls = useAnimation();
 
   useEffect(() => {
@@ -23,79 +23,162 @@ const Projects = () => {
   }, [isInView, mainControls]);
 
   return (
-    <motion.div 
-      ref={ref} 
-      className='w-full py-[4rem] mt-3 px-4 mb-18 text-white'
+    <motion.div
+      ref={ref}
+      className="w-full py-[4rem] mt-3 px-4 mb-18 text-white"
       variants={{
-        hidden: { opacity: 0, y: 75 },
-        visible: { opacity: 1, y: 0 },
+        hidden: {opacity: 0, y: 75},
+        visible: {opacity: 1, y: 0},
       }}
       initial="hidden"
       animate={mainControls}
-      transition={{ duration: 0.8, delay: 0.5 }}
-    >
-      <div className='max-w-[1240px] mx-auto xs:px-10 text-center'>
-        <h1 className='text-white md:text-4xl xs:text-4xl tracking-wide font-bold'>My Projects</h1>
-        <div className='w-[100px] h-[4px] mx-auto bg-sky-300 rounded-md mt-4 mb-12'></div>
+      transition={{duration: 0.8, delay: 0.5}}>
+      <div className="max-w-[1240px] mx-auto xs:px-10 text-center">
+        <h1 className="text-white md:text-4xl xs:text-4xl tracking-wide font-bold">
+          My Projects
+        </h1>
+        <div className="w-[100px] h-[4px] mx-auto bg-sky-300 rounded-md mt-4 mb-12"></div>
       </div>
-        <div className='max-w-[1240px] mx-auto grid md:grid-cols-3 gap-16'>
-        <div className='w-full shadow-xl flex flex-col p-4 rounded-lg bg-slate-800'>
-          <img className='mx-auto rounded-md mt-[-1.5rem] hover:scale-105 duration-300 w-full h-48' src={carolhub} alt='/' />
-          <h2 className='text-2xl font-bold px-4 mt-5'>CarolHub</h2>
-          <p className='text-[14px] px-4 mb-5 italic text-sky-300'>ReactJS, TailwindCSS, FireBase</p>
-          <p className='text-base px-4'>A chat app with Google Sign-In and dynamic chat rooms. </p>
-          <a href='https://carol-hub.web.app/' className='flex justify-end px-4 py-2'>
-            <FontAwesomeIcon icon={faGithub} className='text-2xl hover:text-sky-300' />
+      <div className="max-w-[1240px] mx-auto grid md:grid-cols-3 gap-16">
+        <div className="w-full shadow-xl flex flex-col p-4 rounded-lg bg-slate-800">
+          <img
+            className="mx-auto rounded-md mt-[-1.5rem] hover:scale-105 duration-300 w-full h-48"
+            src={carolhub}
+            alt="/"
+          />
+          <h2 className="text-2xl font-bold px-4 mt-5">CarolHub</h2>
+          <p className="text-[14px] px-4 mb-5 italic text-sky-300">
+            ReactJS, TailwindCSS, FireBase
+          </p>
+          <p className="text-base px-4">
+            A chat app with Google Sign-In and dynamic chat rooms.{" "}
+          </p>
+          <a
+            href="https://carol-hub.web.app/"
+            className="flex justify-end px-4 py-2">
+            <FontAwesomeIcon
+              icon={faGithub}
+              className="text-2xl hover:text-sky-300"
+            />
           </a>
         </div>
-        <div className='w-full shadow-xl flex flex-col p-4 rounded-lg bg-slate-800'>
-          <img className='mx-auto rounded-md mt-[-1.5rem] hover:scale-105 duration-300 w-full h-48' src={bazura} alt='/' />
-          <h2 className='text-2xl font-bold px-4 mt-5'>BAZURA</h2>
-          <p className='text-[14px] px-4 mb-5 italic text-sky-300'>ReactJS, TailwindCSS, NodeJS, Framer-Motion, MySQL, theMealDB API </p>
-          <p className='text-base px-4'>A showcasing website for the Bazura Grill with CRUD capabilities for the admin.</p>
-          <a href='https://github.com/b1gmcdaddy/bazura' className='flex justify-end px-4 py-2'>
-            <FontAwesomeIcon icon={faGithub} className='text-2xl hover:text-sky-300' />
+        <div className="w-full shadow-xl flex flex-col p-4 rounded-lg bg-slate-800">
+          <img
+            className="mx-auto rounded-md mt-[-1.5rem] hover:scale-105 duration-300 w-full h-48"
+            src={bazura}
+            alt="/"
+          />
+          <h2 className="text-2xl font-bold px-4 mt-5">BAZURA</h2>
+          <p className="text-[14px] px-4 mb-5 italic text-sky-300">
+            ReactJS, TailwindCSS, NodeJS, Framer-Motion, MySQL, theMealDB API{" "}
+          </p>
+          <p className="text-base px-4">
+            A showcasing website for the Bazura Grill with CRUD capabilities for
+            the admin.
+          </p>
+          <a
+            href="https://github.com/b1gmcdaddy/bazura"
+            className="flex justify-end px-4 py-2">
+            <FontAwesomeIcon
+              icon={faGithub}
+              className="text-2xl hover:text-sky-300"
+            />
           </a>
         </div>
-        <div className='w-full shadow-xl flex flex-col p-4 rounded-lg bg-slate-800'>
-          <img className='mx-auto rounded-md mt-[-1.5rem] hover:scale-105 duration-300 w-full h-48' src={restos} alt='/' />
-          <h2 className='text-2xl font-bold px-4 mt-5'>Restos v.4.0</h2>
-          <p className='text-[14px] px-4 mb-5 italic text-sky-300'>VueJS, TailwindCSS, ASP.NET Core, Microsoft SQL Server</p>
-          <p className='text-base px-4'>4th version of a restaurant note-tracking app to keep track of potential foodtrips.</p>
-          <a href='https://github.com/b1gmcdaddy/Restos-V.4.0' className='flex justify-end px-4 py-2'>
-            <FontAwesomeIcon icon={faGithub} className='text-2xl hover:text-sky-300' />
+        <div className="w-full shadow-xl flex flex-col p-4 rounded-lg bg-slate-800">
+          <img
+            className="mx-auto rounded-md mt-[-1.5rem] hover:scale-105 duration-300 w-full h-48"
+            src={restos}
+            alt="/"
+          />
+          <h2 className="text-2xl font-bold px-4 mt-5">Restos v.4.0</h2>
+          <p className="text-[14px] px-4 mb-5 italic text-sky-300">
+            VueJS, TailwindCSS, ASP.NET Core, Microsoft SQL Server
+          </p>
+          <p className="text-base px-4">
+            4th version of a restaurant note-tracking app to keep track of
+            potential foodtrips.
+          </p>
+          <a
+            href="https://github.com/b1gmcdaddy/Restos-V.4.0"
+            className="flex justify-end px-4 py-2">
+            <FontAwesomeIcon
+              icon={faGithub}
+              className="text-2xl hover:text-sky-300"
+            />
           </a>
         </div>
-        <div className='w-full shadow-xl flex flex-col p-4 rounded-lg bg-slate-800'>
-          <img className='mx-auto rounded-md mt-[-1.5rem] hover:scale-105 duration-300 w-full h-48' src={saad} alt='/' />
-          <h2 className='text-2xl font-bold px-4 mt-5'>DCISM Inventory System</h2>
-          <p className='text-[14px] px-4 mb-5 italic text-sky-300'>ReactJS, ChakraUI, NodeJS, MySQL</p>
-          <p className='text-base px-4'>Final Project for CIS 2201. Inventory System for the CS/IT/IS Department.</p>
-          <a href='https://github.com/b1gmcdaddy/CIS-2201---Inventory-System' className='flex justify-end px-4 py-2'>
-            <FontAwesomeIcon icon={faGithub} className='text-2xl hover:text-sky-300' />
+        <div className="w-full shadow-xl flex flex-col p-4 rounded-lg bg-slate-800">
+          <img
+            className="mx-auto rounded-md mt-[-1.5rem] hover:scale-105 duration-300 w-full h-48"
+            src={saad}
+            alt="/"
+          />
+          <h2 className="text-2xl font-bold px-4 mt-5">
+            DCISM Inventory System
+          </h2>
+          <p className="text-[14px] px-4 mb-5 italic text-sky-300">
+            ReactJS, ChakraUI, NodeJS, MySQL
+          </p>
+          <p className="text-base px-4">
+            Final Project for CIS 2201. Inventory System for the CS/IT/IS
+            Department.
+          </p>
+          <a
+            href="https://github.com/b1gmcdaddy/CIS-2201---Inventory-System"
+            className="flex justify-end px-4 py-2">
+            <FontAwesomeIcon
+              icon={faGithub}
+              className="text-2xl hover:text-sky-300"
+            />
           </a>
         </div>
-        <div className='w-full shadow-xl flex flex-col p-4 rounded-lg bg-slate-800'>
-          <img className='mx-auto rounded-md mt-[-1.5rem] hover:scale-105 duration-300 w-full h-48' src={interasian} alt='/' />
-          <h2 className='text-2xl font-bold px-4 mt-5'>Real Estate Website</h2>
-          <p className='text-[14px] px-4 mb-5 italic text-sky-300'>PHP, JavaScript, MySQL</p>
-          <p className='text-base px-4'>A website that displays real-estate properties through CRUD functionalities.</p>
-          <a href='https://github.com/b1gmcdaddy/Interasian-Realty-Services' className='flex justify-end px-4 py-2'>
-            <FontAwesomeIcon icon={faGithub} className='text-2xl hover:text-sky-300' />
+        <div className="w-full shadow-xl flex flex-col p-4 rounded-lg bg-slate-800">
+          <img
+            className="mx-auto rounded-md mt-[-1.5rem] hover:scale-105 duration-300 w-full h-48"
+            src={interasian}
+            alt="/"
+          />
+          <h2 className="text-2xl font-bold px-4 mt-5">Real Estate Website</h2>
+          <p className="text-[14px] px-4 mb-5 italic text-sky-300">
+            PHP, JavaScript, MySQL
+          </p>
+          <p className="text-base px-4">
+            A website that displays real-estate properties through CRUD
+            functionalities.
+          </p>
+          <a
+            href="https://github.com/b1gmcdaddy/Interasian-Realty-Services"
+            className="flex justify-end px-4 py-2">
+            <FontAwesomeIcon
+              icon={faGithub}
+              className="text-2xl hover:text-sky-300"
+            />
           </a>
         </div>
-        <div className='w-full shadow-xl flex flex-col p-4 rounded-lg bg-slate-800'>
-          <img className='mx-auto rounded-md mt-[-1.5rem] hover:scale-105 duration-300 w-full h-48' src={parokyano} alt='/' />
-          <h2 className='text-2xl font-bold px-4 mt-5'>PAROKYANO <span className='text-gray-400 text-sm'>(Ongoing)</span></h2>
-          <p className='text-[14px] px-4 mb-5 italic text-sky-300'>ReactJS, MaterialUI, NodeJS, Python, MySQL</p>
-          <p className='text-base px-4'>A web-based, mobile responsive church management system.</p>
-          <a href='' className='flex justify-end px-4 py-2'>
-            <FontAwesomeIcon icon={faGithub} className='text-2xl hover:text-sky-300' />
+        <div className="w-full shadow-xl flex flex-col p-4 rounded-lg bg-slate-800">
+          <img
+            className="mx-auto rounded-md mt-[-1.5rem] hover:scale-105 duration-300 w-full h-48"
+            src={parokyano}
+            alt="/"
+          />
+          <h2 className="text-2xl font-bold px-4 mt-5">PAROKYANO </h2>
+          <p className="text-[14px] px-4 mb-5 italic text-sky-300">
+            ReactJS, MaterialUI, NodeJS, Python, MySQL
+          </p>
+          <p className="text-base px-4">
+            A web-based, mobile responsive church management system.
+          </p>
+          <a href="" className="flex justify-end px-4 py-2">
+            <FontAwesomeIcon
+              icon={faGithub}
+              className="text-2xl hover:text-sky-300"
+            />
           </a>
         </div>
       </div>
     </motion.div>
   );
-}
+};
 
 export default Projects;
